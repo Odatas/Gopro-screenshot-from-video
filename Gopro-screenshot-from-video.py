365373ff9005146debb4e1d02328534cdb00fb82
Odatas <46987568+Odatas@users.noreply.github.com>
2020-05-27 00:44:36 +0200

    Added gps and date to Exif
    GPS and Date will be now stored directly into the exif tag.

--- Gopro-screenshot-from-video.py
+++ Gopro-screenshot-from-video.py
@@ -3,6 +3,9 @@ import wx.media
 import subprocess
 import gopro2gpx
 import xmltodict
+import piexif
+from fractions import Fraction
+from datetime import datetime
 
 
 """
@@ -13,6 +16,12 @@ Created on Fri May 22 19:42:18 2020
 
 
 class VideoCapture(wx.Frame):
+    """
+    mediapath as str path to the mp4 medium
+
+    opens a media widget to play the video. Extracts gps from the video a
+    and pares it to the screenshot that gets created
+    """
     def __init__(self, mediapath, parent=None):
         wx.Frame.__init__(self, parent=parent, size=(1000, 1000))
         self.picturenumber = 0
@@ -20,30 +29,30 @@ class VideoCapture(wx.Frame):
         self.media = mediapath
         self.Knopp = wx.Button(self.panel, label='Save Screenshot')
         self.Knopp.Bind(wx.EVT_BUTTON, self.screenshot)
-        self.testMedia = wx.media.MediaCtrl(self.panel, 
-                                            style=wx.SIMPLE_BORDER, 
+        self.testMedia = wx.media.MediaCtrl(self.panel,
+                                            style=wx.SIMPLE_BORDER,
                                             szBackend=wx.media.MEDIABACKEND_WMP10)
-        wx.media.MediaCtrl.ShowPlayerControls(self.testMedia,flags=wx.media.MEDIACTRLPLAYERCONTROLS_DEFAULT)
+        wx.media.MediaCtrl.ShowPlayerControls(self.testMedia,
+                                              flags=wx.media.MEDIACTRLPLAYERCONTROLS_DEFAULT)
 
         self.testMedia.Bind(wx.media.EVT_MEDIA_LOADED, self.play)
         self.testMedia.Bind(wx.media.EVT_MEDIA_FINISHED, self.quit)
 
-        self.sizer=wx.BoxSizer(wx.VERTICAL)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
 
-        self.sizer.Add(self.Knopp,0,wx.ALL,5)
-        self.sizer.Add(self.testMedia,0,wx.ALL,5)
-
+        self.sizer.Add(self.Knopp, 0, wx.ALL, 5)
+        self.sizer.Add(self.testMedia, 0, wx.ALL, 5)
 
         self.panel.SetSizer(self.sizer)
 
         try:
-            self.rawgps=gopro2gpx.extract(self.media,skip=False, verbose=2)
-            self.gpsdict=xmltodict.parse(self.rawgps)
-            self.gps=True
+            self.rawgps = gopro2gpx.extract(self.media, skip=False)
+            self.gpsdict = xmltodict.parse(self.rawgps)
+            self.gps = True
         except Exception as e:
-            wx.MessageBox('Konnte GPS Daten nicht lesen. Error: '+str(e), 'Èrror')
-            self.gps=False
-
+            wx.MessageBox('Konnte GPS Daten nicht lesen. Error: '
+                          + str(e), 'Èrror')
+            self.gps = False
 
         if self.testMedia.Load(self.media):
             pass
@@ -53,44 +62,133 @@ class VideoCapture(wx.Frame):
 
         self.Show()
 
-
-
-
     def play(self, event):
         self.testMedia.Play()
 
-    def screenshot(self,event):
-        millis=self.testMedia.Tell()+200
-        seconds=(millis/1000)%60
+    def screenshot(self, event):
+        millis = self.testMedia.Tell() + 200
+        seconds = (millis / 1000) % 60
         seconds = int(seconds)
-        minutes=(millis/(1000*60))%60
+        minutes = (millis / (1000 * 60)) % 60
         minutes = int(minutes)
-        hours=(millis/(1000*60*60))%24
-        hours=int(hours)
-        time=str(hours).zfill(2)+':'+str(minutes).zfill(2)+':'+str(seconds).zfill(2)
+        hours = (millis / (1000 * 60 * 60)) % 24
+        hours = int(hours)
+        time = str(hours).zfill(2) + ':' + str(minutes).zfill(2) + ':' + str(seconds).zfill(2)
 
-        onlyseconds=int(millis/1000)
+        onlyseconds = int(millis / 1000)
+        if onlyseconds == 0:
+            onlyseconds = 1
 
         if self.gps:
 
-            lat='Lat'+self.gpsdict["gpx"]["trk"]["trkseg"]["trkpt"][onlyseconds-1]["@lat"]
-            long='Long'+self.gpsdict["gpx"]["trk"]["trkseg"]["trkpt"][onlyseconds-1]["@lon"]
+            lat = self.gpsdict["gpx"]["trk"]["trkseg"]["trkpt"][onlyseconds-1]["@lat"]
+            long = self.gpsdict["gpx"]["trk"]["trkseg"]["trkpt"][onlyseconds-1]["@lon"]
+            ele = self.gpsdict["gpx"]["trk"]["trkseg"]["trkpt"][onlyseconds-1]["ele"]
+            if float(ele) < 0:
+                ele=0
+            print("ele: " + str(ele))
+            #timeformat 2019-05-10T13:17:28Z
+            picturetime = self.gpsdict["gpx"]["trk"]["trkseg"]["trkpt"][onlyseconds-1]["time"]
+            format_str = '%Y-%m-%dT%H:%M:%SZ'
+            datetime_obj = datetime.strptime(picturetime, format_str)
+            name ='image'+str(self.picturenumber) + '.jpg'
+            command='ffmpeg -y -ss '+time+' -i "'+self.media+'" -frames:v 1 -q:v 2 ' + name
+            print(command)
+            print('\n')
+            print(subprocess.run(command))
+            self.set_gps_location(name,float(lat),float(long),float(ele),datetime_obj)
         else:
-            lat=''
-            long=''
+            name='image'+str(self.picturenumber) + 'no geodata' +'.jpg'
+            command='ffmpeg -y -ss '+time+' -i "'+self.media+'" -frames:v 1 -q:v 2 ' + name
+            print(subprocess.run(command))
+            
+            command='ffmpeg -y -ss '+time+' -i "'+self.media+'" -frames:v 1 -q:v 2 image'+str(self.picturenumber)+'-'+lat+'-'+long+'.jpg'
 
-
-        folder=r'F:\\'
-
-        command='ffmpeg -y -ss '+time+' -i "'+self.media+'" -frames:v 1 -q:v 2 image'+str(self.picturenumber)+'-'+lat+'-'+long+'.jpg'
-        print(subprocess.run(command))
+        
         self.picturenumber+=1
+        
+        
+    def to_deg(self, value, loc):
+        """convert decimal coordinates into degrees, munutes and seconds tuple
+        Keyword arguments: value is float gps-value, loc is direction list ["S", "N"] or ["W", "E"]
+        return: tuple like (25, 13, 48.343 ,'N')
+        """
+        if value < 0:
+            loc_value = loc[0]
+        elif value > 0:
+            loc_value = loc[1]
+        else:
+            loc_value = ""
+        abs_value = abs(value)
+        deg =  int(abs_value)
+        t1 = (abs_value-deg)*60
+        min = int(t1)
+        sec = round((t1 - min)* 60, 5)
+        return (deg, min, sec, loc_value)
+
+
+    def change_to_rational(self, number):
+        """convert a number to rantional
+        Keyword arguments: number
+        return: tuple like (1, 2), (numerator, denominator)
+        """
+        f = Fraction(str(number))
+        return (f.numerator, f.denominator)
+
+
+    def set_gps_location(self, file_name, lat, lng, altitude,datetime_obj):
+        """Adds GPS position as EXIF metadata
+        Keyword arguments:
+        file_name -- image file
+        lat -- latitude (as float)
+        lng -- longitude (as float)
+        altitude -- altitude (as float)
+        """
+        lat_deg = self.to_deg(lat, ["S", "N"])
+        lng_deg = self.to_deg(lng, ["W", "E"])
+    
+        exiv_lat = (self.change_to_rational(lat_deg[0]), self.change_to_rational(lat_deg[1]), self.change_to_rational(lat_deg[2]))
+        exiv_lng = (self.change_to_rational(lng_deg[0]), self.change_to_rational(lng_deg[1]), self.change_to_rational(lng_deg[2]))
+    
+        gps_ifd = {
+            piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
+            piexif.GPSIFD.GPSAltitudeRef: 1,
+            piexif.GPSIFD.GPSAltitude: self.change_to_rational(round(altitude)),
+            piexif.GPSIFD.GPSLatitudeRef: lat_deg[3],
+            piexif.GPSIFD.GPSLatitude: exiv_lat,
+            piexif.GPSIFD.GPSLongitudeRef: lng_deg[3],
+            piexif.GPSIFD.GPSLongitude: exiv_lng,
+            
+        }
+        print(datetime_obj.strftime("%Y:%m:%d %H:%M:%S"))
+        exif_ifd = {
+            piexif.ExifIFD.DateTimeOriginal: datetime_obj.strftime("%Y:%m:%d %H:%M:%S"),
+            }
+        # exif_dict = {"GPS": gps_ifd}
+        # exif_bytes = piexif.dump(exif_dict)
+        # piexif.insert(exif_bytes, file_name)
+        
+        exif_exif = {"Exif":exif_ifd}
+        gps_exif = {"GPS": gps_ifd}
+
+        # get original exif data first!
+        exif_data = piexif.load(file_name)
+    
+        # update original exif data to include GPS tag
+        exif_data.update(gps_exif)
+        exif_data.update(exif_exif)
+        exif_bytes = piexif.dump(exif_data)
+    
+        piexif.insert(exif_bytes, file_name)
 
     def quit(self, event):
         self.Destroy()
 
 
 class mainWindow(wx.Frame):
+    """
+    Creates Bare Bone Video Selection Window
+    """
     def __init__(self):
         wx.Frame.__init__(self, None)
         self.panel = wx.Panel(self)
@@ -126,6 +224,7 @@ class mainWindow(wx.Frame):
         else:
             wx.MessageBox('Unbekannter Fehler beim Auswählen des Ordners. Ist der Ordner Verfügbar?', 'Èrror',)
         dialog.Destroy()
+
 
 
 if __name__ == '__main__':
